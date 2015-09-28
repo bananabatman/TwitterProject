01970642d3f5fe5dbb8cf3ecf60d5922436083a3
HannaJoline <jolinebergsjo@gmail.com>
2015-09-28 14:51:21 +0200

    added function skeleton to controller.js

--- public/javascripts/Controller.js
+++ public/javascripts/Controller.js
@@ -11,3 +11,46 @@ function initMap(){
     map.addLayer(osm);
     }
     
+
+filterOnCategory(){	
+	//send request to model containing category as parameter
+	//=> in model(index) selected query is executed returning hashtags where category = input
+}
+
+
+highlightOption(){	
+	//show that hashtag is chosen and dull out the other hashtags updateMarkers
+	//if top5 clicked clear everything/update markers
+}
+
+
+fullScreenMap(){	
+	//makes map full screen/normal size
+}
+
+
+updateMarkers(object:hashtags){	
+	//function update top 5:
+	//remove old hashtags and replace with new hashtags
+	//Input: objects with lat- long-attributes
+}
+
+updateMap(position){
+	//move to cntre and zoom for location and district	
+	//if position = gamla stan -> go to position
+	//if position = my location -> {get location}
+}
+
+determineTopFive(category){	
+}
+
+getUserLocations(){	
+}
+
+insertUserLocation(){	
+}	
+
+
+
+
+
