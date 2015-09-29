b79deb5e72f57aecc796c6280702aed433294d49
bananabatman <ellas@kth.se>
2015-09-29 14:35:21 +0200

    Bug fixes and starting on layout

--- public/javascripts/Controller.js
+++ public/javascripts/Controller.js
@@ -12,43 +12,43 @@ function initMap(){
     }
     
 
-filterOnCategory(){	
+function filterOnCategory(){	
 	//send request to model containing category as parameter
 	//=> in model(index) selected query is executed returning hashtags where category = input
 	//=> Index.js calls updateMarkers()
 }
 
 
-highlightOption(){	
+function highlightOption(){	
 	//show that hashtag is chosen and dull out the other hashtags updateMarkers
 	//if top5 clicked clear everything/update markers
 }
 
 
-fullScreenMap(){	
+function fullScreenMap(){	
 	//makes map full screen/normal size
 }
 
 
-updateMarkers(object:hashtags){	
+function updateMarkers(hashtags){	
 	//function update top 5:
 	//remove old hashtags and replace with new hashtags
 	//Input: objects with lat- long-attributes
 }
 
-updateMap(position){
+function updateMap(position){
 	//move to cntre and zoom for location and district	
 	//if position = gamla stan -> go to position
 	//if position = my location -> {get location}
 }
 
-determineTopFive(category){	
+function determineTopFive(category){	
 }
 
-getUserLocations(){	
+function getUserLocations(){	
 }
 
-insertUserLocation(){	
+function insertUserLocation(){	
 }	
 
 
