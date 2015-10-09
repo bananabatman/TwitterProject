babe08a8d92cdfa9c30f4d0f55e1e8ea70f9d885
lightitupra <jenniy@kth.se>
2015-10-09 11:42:44 +0200

    update everyone else work
    add from master

--- dbcredentials/credentials.js
+++ dbcredentials/credentials.js
@@ -1,8 +1,8 @@
 var pg = require('pg');
 
-var host ='localhost';
+var host ='130.237.64.7';
 var port ='5432';
-var dbName = 'ProjWebGIS';
+var dbName = 'webgis_lab3';
 var username = 'postgres';
 var password = 'password';
 
@@ -16,8 +16,8 @@ var client = new pg.Client(connectionString);
 client.connect();
 
 // these queries will only be run once, for initialization
-var queryCreateUserTable = client.query('CREATE TABLE if not exists tweets (tweet_id serial not null, hashtag text, category text, latitude double precision, longitude double precision)'); 
-var queryCreateMarkerTable = client.query('CREATE TABLE if not exists stalker (marker_id serial not null, user_id bigint not null, latitude double precision, longitude double precision)');
+// var queryCreateUserTable = client.query('CREATE TABLE if not exists user_table (user_id serial not null, email text, password text, isAdmin boolean default false)'); 
+// var queryCreateMarkerTable = client.query('CREATE TABLE if not exists marker_table (marker_id serial not null, user_id bigint not null, latitude double precision, longitude double precision)');
 
 //Execute queries
 queryCreateUserTable.on('end', function() {
